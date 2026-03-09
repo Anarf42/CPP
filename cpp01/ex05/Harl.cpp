a5d1fae6fed2ad5272f20c7b2c3e0e6b43b87138
Ana Ruiz <anaraf.42@gmail.com>
2026-03-09 14:51:41 +0000

    finish

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -12,22 +12,22 @@
 
 #include "Harl.hpp"
 
-void debug(void)
+void Harl::debug(void)
 {
-	std::cout << "hola" << std::endl;
+	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n" << std::endl;
 }
-void	info(void)
+void	Harl::info(void)
 {
-	std::cout << "mundo" << std::endl;
+	std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
-void	warning(void)
+void	Harl::warning(void)
 {
-	std::cout << "buenos" << std::endl;	
+	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n" << std::endl;	
 }
-void	error(void)
+void	Harl::error(void)
 {
-	std::cout << "dias" << std::endl;
+	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
 void	Harl::complain(std::string level)
