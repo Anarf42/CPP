fe26414144239d607cca0644fd354d89f2e102d5
Ana Ruiz <anaraf.42@gmail.com>
2026-04-24 13:57:38 +0000

    pendiente de terminar

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,11 +6,24 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 10:59:19 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/22 11:00:13 by codespace        ###   ########.fr       */
+/*   Updated: 2026/04/24 13:44:14 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+    protected:
+        std::string _Name;
+        int _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string& name);
+        Bureaucrat(const Bureaucrat& copy);
+        Bureaucrat& operator=(const Bureaucrat& copy);
+        ~Bureaucrat();
+}
 
 #endif
